df6431c884578f1ed4813140720de7399ab30cb6
Franklin Cabrera <frankcabrera2.718@gmail.com>
2025-08-08 08:54:41 -0600

    Agregar archivos de proyecto.

---
+++ SistemaPagos.Server/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    //aquí se definen las entidades del modelo: DbSet<modelo>
+}
