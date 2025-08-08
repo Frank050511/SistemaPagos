62171aeaffb0636e60b0dff3cb1141c5243fb545
Franklin Cabrera <frankcabrera2.718@gmail.com>
2025-08-08 16:31:56 -0600

    creacion del modelo para Planilla

---
+++ SistemaPagos.Server/Models/PlanillaModel.cs
@@ -0,0 +1,26 @@
+﻿using SistemaPagos.Server.Models;
+using System.Collections.Generic;   
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+
+public class PlanillaModel
+ {
+    [Key]
+    public int Id_planilla { get; set; }
+
+    [StringLength(30)]
+    public string Nombre_planilla { get; set; } = string.Empty;
+
+    public DateTime Fecha_carga { get; set; } = DateTime.Now;
+    public string Ruta_archivo { get; set; } = string.Empty;
+    public bool Activo { get; set; } = true;
+    public DateTime Fecha_corte { get; set; }
+
+    [ForeignKey("Id_usuario")]
+    public UsuarioModel? Usuario { get; set; } 
+    public int Id_usuario { get; set; }
+
+   
+}
+
