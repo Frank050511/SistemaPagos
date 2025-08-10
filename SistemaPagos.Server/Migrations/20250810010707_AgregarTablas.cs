97a5521c64a462a9361e9de2a274dac29a0e4cdd
Franklin Cabrera <frankcabrera2.718@gmail.com>
2025-08-09 19:33:38 -0600

    Creacion de migraciones

---
+++ SistemaPagos.Server/Migrations/20250810010707_AgregarTablas.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaPagos.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarTablas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    IdUsuario = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CodigoEmpleado = table.Column<string>(type: "varchar(10)", maxLength: 10, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Nombre = table.Column<string>(type: "varchar(150)", maxLength: 150, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Clave = table.Column<string>(type: "varchar(8)", maxLength: 8, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Activo = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    EsAdmin = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.IdUsuario);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Planillas",
+                columns: table => new
+                {
+                    IdPlanilla = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    NombrePlanilla = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    FechaCarga = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    RutaArchivo = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Activo = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    FechaCorte = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    IdUsuario = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Planillas", x => x.IdPlanilla);
+                    table.ForeignKey(
+                        name: "FK_Planillas_Usuarios_IdUsuario",
+                        column: x => x.IdUsuario,
+                        principalTable: "Usuarios",
+                        principalColumn: "IdUsuario",
+                        onDelete: ReferentialAction.Restrict);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Detalles",
+                columns: table => new
+                {
+                    IdDetalle = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    SalarioBruto = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    Isss = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    Afp = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    Renta = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    SalarioNeto = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    IdPlanilla = table.Column<int>(type: "int", nullable: false),
+                    IdUsuario = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Detalles", x => x.IdDetalle);
+                    table.ForeignKey(
+                        name: "FK_Detalles_Planillas_IdPlanilla",
+                        column: x => x.IdPlanilla,
+                        principalTable: "Planillas",
+                        principalColumn: "IdPlanilla",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Detalles_Usuarios_IdUsuario",
+                        column: x => x.IdUsuario,
+                        principalTable: "Usuarios",
+                        principalColumn: "IdUsuario",
+                        onDelete: ReferentialAction.Restrict);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Notificaciones",
+                columns: table => new
+                {
+                    IdNotificacion = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Mensaje = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    FechaEnvio = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Leida = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    IdUsuario = table.Column<int>(type: "int", nullable: false),
+                    IdPlanilla = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notificaciones", x => x.IdNotificacion);
+                    table.ForeignKey(
+                        name: "FK_Notificaciones_Planillas_IdPlanilla",
+                        column: x => x.IdPlanilla,
+                        principalTable: "Planillas",
+                        principalColumn: "IdPlanilla",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Notificaciones_Usuarios_IdUsuario",
+                        column: x => x.IdUsuario,
+                        principalTable: "Usuarios",
+                        principalColumn: "IdUsuario",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Detalles_IdPlanilla",
+                table: "Detalles",
+                column: "IdPlanilla");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Detalles_IdUsuario",
+                table: "Detalles",
+                column: "IdUsuario");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notificaciones_IdPlanilla",
+                table: "Notificaciones",
+                column: "IdPlanilla");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notificaciones_IdUsuario",
+                table: "Notificaciones",
+                column: "IdUsuario");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Planillas_IdUsuario",
+                table: "Planillas",
+                column: "IdUsuario");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Detalles");
+
+            migrationBuilder.DropTable(
+                name: "Notificaciones");
+
+            migrationBuilder.DropTable(
+                name: "Planillas");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
